5ec00d69dfd26164dbed8ea0f005ff4f335d306c
jinsu009 <dnqlthsus9@gmail.com>
2026-08-11 13:19:29 +0900

    사용자 todo controller 수정

--- src/main/java/com/member/membermanagement/user/controller/UserController.java
+++ src/main/java/com/member/membermanagement/user/controller/UserController.java
@@ -8,7 +8,6 @@ import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
 
 import java.util.HashMap;
 import java.util.Map;
